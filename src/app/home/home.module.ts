2a05c61d7316360fbfacf935b08442ffa9d709ce
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-07-07 10:23:57 -0300

    curso 02 | aula 04 completo

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,10 @@
-import { SignInComponent } from './signin/signin.component';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [SignInComponent],
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class HomeModule {}
