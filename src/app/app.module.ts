51e29535c384826f3c496644ca16edfb7ce2e5b3
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-07-12 09:02:39 -0300

    curso 02 | capitulo 02 completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
 import { HomeModule } from './home/home.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { HomeModule } from './home/home.module';
     AppRoutingModule,
     ErrorsModule,
     HomeModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
